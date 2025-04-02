6c8ec30e4a35329610abe274a6ebd5d626dbb8a6
Renzonius <48992705+Renzonius@users.noreply.github.com>
2025-04-02 15:17:05 -0300

    Un paso mas

--- Objetos.py
+++ Objetos.py
@@ -1,5 +1,5 @@
 import qrcode  # type: ignore # Importa la librería para generar códigos QR
 
-url = "https://tusitio.com/modelo3d"  # Enlace al modelo 3D
+url = "https://renzonius.github.io/museo_AR/"  # Enlace al modelo 3D
 qr = qrcode.make(url)  # Crea el código QR con la URL
 qr.save("modelo3d_qr.png")  # Guarda la imagen del código QR
